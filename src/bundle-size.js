cd6093b7e0c171ca01fd561d285e4ed7af5b5f50
UmairShahzad <18100099@lums.edu.pk>
2020-04-27 15:51:06 +0500

    test bundle_size workflow

---
+++ src/bundle-size.js
@@ -0,0 +1,45 @@
+let { constants, createBrotliCompress } = require('zlib');
+let fs = require('fs');
+function brotliSize(path) {
+  return new Promise((resolve, reject) => {
+    let size = 0;
+    let pipe = fs.createReadStream(path).pipe(
+      createBrotliCompress({
+        params: {
+          [constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      }),
+    );
+    pipe.on('error', reject);
+    pipe.on('data', buf => {
+      size += buf.length;
+    });
+    pipe.on('end', () => {
+      resolve(size);
+    });
+  });
+}
+fs.readdir('build/static/js', async (err, files) => {
+  if (!err) {
+    const fileSizes = {};
+    await Promise.all(
+      files.map(async filePath => {
+        if (filePath.endsWith('.chunk.js')) {
+          fileSizes[filePath.split('.')[0]] = await brotliSize(
+            `build/static/js/${filePath}`,
+          );
+        }
+      }),
+    );
+    fs.readFile('baseline.json', (err, data) => {
+      if (!err) {
+        const baseline = JSON.parase(data);
+        Object.keys(fileSizes).forEach(file => {
+          if (fileSizes[file] !== baseline[file]) {
+          }
+        });
+      }
+    });
+    console.log(JSON.stringify(fileSizes));
+  }
+});
