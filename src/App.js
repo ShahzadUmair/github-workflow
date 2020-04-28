1a838fb4b92ca74fc1351d0c869b7a7162c0edbc
UmairShahzad <18100099@lums.edu.pk>
2020-04-28 17:53:32 +0500

    increase

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - Lorem ipsum dolor sit amet, consectetur adipiscing elit :O. 
         </a>
       </header>
     </div>
