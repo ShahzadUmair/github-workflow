9ddd0a23b3d0e29ef37aaa411f613c824408e18c
UmairShahzad <18100099@lums.edu.pk>
2020-04-28 14:34:14 +0500

    edit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - create-react-app-test
         </a>
       </header>
     </div>
