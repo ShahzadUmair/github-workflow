2b5c5073befa79be80e0d5fcf0c0cde88387b32d
UmairShahzad <18100099@lums.edu.pk>
2020-04-28 13:12:04 +0500

    size test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - create-react-app
+          Learn React Test
         </a>
       </header>
     </div>
